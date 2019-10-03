971ea78c76ff390848549b4de788d977382dab0d
Thiago Augusto Barros de Almeida <thy-almeida@hotmail.com>
2019-10-03 19:37:55 +0930

    Test OSM call and Revit init Command

--- Osm.Revit/Commands/TestCommand.cs
+++ Osm.Revit/Commands/TestCommand.cs
@@ -1,19 +1,34 @@
 ﻿using Autodesk.Revit.DB;
 using Autodesk.Revit.UI;
-using System;
-using System.Collections.Generic;
+using Osm.Revit.Models;
+using Osm.Revit.Services;
+using OsmSharp.Streams;
+using System.IO;
+using OsmSharp.Tags;
+using Autodesk.Revit.Attributes;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Osm.Revit.Commands
 {
+    [Transaction(TransactionMode.Manual)]
     public class TestCommand : IExternalCommand
     {
         public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
         {
             UIDocument uidoc = commandData.Application.ActiveUIDocument;
             Document doc = uidoc.Document;
+
+            var httpService = new HttpService();
+            var source = httpService.GetMapStream(new MapBounds
+            {
+                Left = -74.06062f,
+                Bottom = 40.79735f,
+                Right = -74.05220f,
+                Top = 40.80097f,
+            });
+
+            var everything = source.FilterNodes(n => true).ToList();
+            var buildings = everything.Where(n => n.Tags.IsTrue("buildings")).ToList();
 
             return Result.Succeeded;
         }
---
+++ Osm.Revit/Models/MapBounds.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Osm.Revit.Models
+{
+    public class MapBounds
+    {
+        public double Left { get; set; }
+        public double Top { get; set; }
+        public double Right { get; set; }
+        public double Bottom { get; set; }
+    }
+}
