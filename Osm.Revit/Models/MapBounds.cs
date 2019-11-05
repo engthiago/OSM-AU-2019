c32af4b06621888fb74d0bcd1665bbfe7ce0a207
Rahul Bhobe <Rahul.Bhobe@autodesk.com>
2019-11-05 04:15:51 -0500

    Added Deserialization/Serialization for MapBounds.

--- Osm.Revit/Models/MapBounds.cs
+++ Osm.Revit/Models/MapBounds.cs
@@ -1,16 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Newtonsoft.Json;
 
 namespace Osm.Revit.Models
 {
     public class MapBounds
     {
+        [JsonProperty(PropertyName = "left")]
         public double Left { get; set; }
+        [JsonProperty(PropertyName = "top")]
         public double Top { get; set; }
+        [JsonProperty(PropertyName = "right")]
         public double Right { get; set; }
+        [JsonProperty(PropertyName = "bottom")]
         public double Bottom { get; set; }
+        public static MapBounds Deserialize(string json) => JsonConvert.DeserializeObject<MapBounds>(json);
+        public static string Serialize(MapBounds mapBounds) => JsonConvert.SerializeObject(mapBounds);
     }
 }
