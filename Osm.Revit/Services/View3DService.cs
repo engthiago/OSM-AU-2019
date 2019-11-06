524cc7013bffccf1c5430ed153b824449dc86f1f
Rahul Bhobe <Rahul.Bhobe@autodesk.com>
2019-11-06 08:26:14 -0500

    Created missing 3d view and made default startup view.

--- Osm.Revit/Services/View3DService.cs
+++ Osm.Revit/Services/View3DService.cs
@@ -15,15 +15,31 @@ namespace Osm.Revit.Services
             this.osmStore = osmStore;
         }
 
+        private static View3D Get3DView(Document doc) => doc.ActiveView as View3D ?? new FilteredElementCollector(doc)
+            .OfCategory(BuiltInCategory.OST_Views)
+            .OfClass(typeof(View3D))
+            .Cast<View3D>()
+            .FirstOrDefault(v => v.IsTemplate == false && v.IsPerspective == false);
+
+        private static ElementId GetViewFamilyTypeId(Document doc) => new FilteredElementCollector(doc)
+            .OfClass(typeof(ViewFamilyType))
+            .Cast<ViewFamilyType>()
+            .FirstOrDefault<ViewFamilyType>(x => ViewFamily.ThreeDimensional == x.ViewFamily).Id;
+
+        private static View3D GetOrCreate3DView(Document doc)
+        {
+            var view3D = Get3DView(doc) ?? View3D.CreateIsometric(doc, GetViewFamilyTypeId(doc));
+            view3D.Name = "OsmDemoView";
+
+            var settings = new FilteredElementCollector(doc).OfClass(typeof(StartingViewSettings)).Single() as StartingViewSettings;
+            settings.ViewId = view3D.Id;
+
+            return view3D;
+        }
+
         public View3D Run(Document doc)
         {
-            var view3d = new FilteredElementCollector(doc)
-                .OfCategory(BuiltInCategory.OST_Views)
-                .OfClass(typeof(View3D))
-                .Cast<View3D>()
-                .FirstOrDefault(v => v.IsTemplate == false && v.IsPerspective == false);
-
-            if (view3d == null) return null;
+            var view3d = GetOrCreate3DView(doc);
 
             view3d.IsSectionBoxActive = true;
 
