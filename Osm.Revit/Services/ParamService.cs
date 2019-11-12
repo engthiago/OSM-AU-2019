1d3209e945875366d698323791aac2eb5b67ae5c
Thiago Augusto Barros de Almeida <thy-almeida@hotmail.com>
2019-11-12 21:47:22 +1030

    Setting up Shared params

--- Osm.Revit/Services/ParamService.cs
+++ Osm.Revit/Services/ParamService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Autodesk.Revit.DB;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,11 +12,13 @@ namespace Osm.Revit.Services
         static public string ParamFile => "OSM.Shared.txt";
 
         static readonly string sourceFile = "OSM.Shared.Source.json";
+        static readonly string defGroupName = "OSM";
 
         // Shared
 
         static readonly string nameParamName = "Name";
-        static readonly string nameParmGUID = "E513F3CE-5784-4936-AB26-CBE1C2212CB3";
+        static readonly string nameParamGUID = "E513F3CE-5784-4936-AB26-CBE1C2212CB3";
+        static readonly string nameParamDescription = "The common name for Open Street Map Elements";
 
         // Building
 
@@ -35,25 +38,124 @@ namespace Osm.Revit.Services
 
         static readonly string streetUsageParamName = "Usage";
         static readonly string streetUsageParamGUID = "18C7DEEF-6FDC-4B31-BE2A-F47EC9787D40";
+        static readonly string streetUsageDescription = "Usage of the street, refered as 'highway' by OSM";
 
         static readonly string streetTypeParamName = "Type";
         static readonly string streetTypeParamGUID = "2F314F17-DB21-430C-BD3F-C6B7029C28F9";
+        static readonly string streetTypeDescription = "Type of the street, refered as 'tiger:name_type' by OSM";
 
         static readonly string streetLanesParam = "Lanes";
         static readonly string streetLanesParamGUID = "965B8243-1D52-4E74-BDF7-62E9437615E7";
+        static readonly string streetLanesDescription = "Total lanes of the street";
 
         static readonly string streetMaxSpeedParam = "Max Speed";
         static readonly string streetMaxSpeedParamGUID = "F1195882-5C16-4B35-8D73-35E8F3C05A42";
+        static readonly string streetMaxSpeedDescription = "Maximum speed of the street, in Km/h";
 
         static readonly string streetSurfaceParam = "Surface";
         static readonly string streetSurfaceParamGUID = "7F558884-6311-4186-8CCC-CA4E9989469B";
+        static readonly string streetSurfaceParamDescription = "Pavement type of the street";
 
 
         //static readonly string name
 
         public ParamService()
         {
+            this.SetupFile(ParamFile);
         }
+
+        private void SetupFile(string filename)
+        {
+            try
+            {
+                string directory = System.IO.Path.GetDirectoryName(filename);
+                if (!System.IO.Directory.Exists(directory))
+                {
+                    System.IO.Directory.CreateDirectory(directory);
+                }
+
+                if (System.IO.File.Exists(filename))
+                {
+                    System.IO.File.Delete(filename);
+                }
+
+                var file = System.IO.File.Create(filename);
+                file.Close();
+                file.Dispose();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error setting up shared file: " + e.Message);
+            }
+        }
+
+        private void CreateStreetParameters(Document doc)
+        {
+            // Categories
+            CategorySet catSet = new CategorySet();
+            Category cat = Category.GetCategory(doc, BuiltInCategory.OST_Roads);
+            catSet.Insert(cat);
+
+            Binding binding = new InstanceBinding(catSet);
+
+            var app = doc.Application;
+            app.SharedParametersFilename = ParamFile;
+            DefinitionFile defFile = app.OpenSharedParameterFile();
+            DefinitionGroup defGroup = defFile.Groups.FirstOrDefault(d => d.Name == defGroupName);
+
+            if (defGroup == null)
+            {
+                defGroup = defFile.Groups.Create(defGroupName);
+            }
+
+            var options = new ExternalDefinitionCreationOptions(nameParamName, ParameterType.Text);
+            options.GUID = new Guid(nameParamGUID);
+            options.Description = nameParamDescription;
+            CreateParam(doc, binding, defGroup, options);
+
+            options = new ExternalDefinitionCreationOptions(streetUsageParamName, ParameterType.Text);
+            options.GUID = new Guid(streetUsageParamGUID);
+            options.Description = streetUsageDescription;
+            CreateParam(doc, binding, defGroup, options);
+
+            options = new ExternalDefinitionCreationOptions(streetTypeParamName, ParameterType.Text);
+            options.GUID = new Guid(streetTypeParamGUID);
+            options.Description = streetTypeDescription;
+            CreateParam(doc, binding, defGroup, options);
+
+            options = new ExternalDefinitionCreationOptions(streetLanesParam, ParameterType.Integer);
+            options.GUID = new Guid(streetLanesParamGUID);
+            options.Description = streetLanesDescription;
+            CreateParam(doc, binding, defGroup, options);
+
+            options = new ExternalDefinitionCreationOptions(streetMaxSpeedParam, ParameterType.Integer);
+            options.GUID = new Guid(streetMaxSpeedParamGUID);
+            options.Description = streetMaxSpeedDescription;
+            CreateParam(doc, binding, defGroup, options);
+
+            options = new ExternalDefinitionCreationOptions(streetSurfaceParam, ParameterType.Text);
+            options.GUID = new Guid(streetSurfaceParamGUID);
+            options.Description = streetSurfaceParamDescription;
+            CreateParam(doc, binding, defGroup, options);
+        }
+
+        private void CreateParam(Document doc, Binding binding, DefinitionGroup defGroup, ExternalDefinitionCreationOptions options)
+        {
+            var definition = this.CreateDefinition(defGroup, options);
+            doc.ParameterBindings.Insert(definition, binding, BuiltInParameterGroup.PG_IDENTITY_DATA);
+        }
+
+        private Definition CreateDefinition(DefinitionGroup defGroup, ExternalDefinitionCreationOptions options)
+        {
+            Definition definition = defGroup.Definitions.FirstOrDefault(d => d.Name == options.Name);
+            if (definition == null)
+            {
+                definition = defGroup.Definitions.Create(options);
+            }
+
+            return definition;
+        }
+
 
 
     }
